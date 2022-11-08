2551e274e6c5dc3ee13aa559aac4a5e9547ea6c3
Majeed-hub <majeedbkl65@gmail.com>
2022-11-09 00:46:39 +0530

    final change

---
+++ build/precache-manifest.f04752db67bc51bd74188501473a57d0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "35a90ad8e567ae0374e9",
+    "url": "/Majeed-hub/react-app-2/static/css/main.01509f7e.chunk.css"
+  },
+  {
+    "revision": "35a90ad8e567ae0374e9",
+    "url": "/Majeed-hub/react-app-2/static/js/main.35a90ad8.chunk.js"
+  },
+  {
+    "revision": "8f2f4211b0891106f089",
+    "url": "/Majeed-hub/react-app-2/static/js/1.8f2f4211.chunk.js"
+  },
+  {
+    "revision": "ac4de3e2a0e25d090f27",
+    "url": "/Majeed-hub/react-app-2/static/js/2.ac4de3e2.chunk.js"
+  },
+  {
+    "revision": "c81581b49ca74a008a03",
+    "url": "/Majeed-hub/react-app-2/static/js/runtime~main.c81581b4.js"
+  },
+  {
+    "revision": "58070facc8aba6e30de2829c4ede8edd",
+    "url": "/Majeed-hub/react-app-2/static/media/image.58070fac.jpg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Majeed-hub/react-app-2/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "634af39c06a06ba61e39e9cb94edac38",
+    "url": "/Majeed-hub/react-app-2/static/media/profile.634af39c.png"
+  },
+  {
+    "revision": "bd19b8f33b082080cfdefd2115043094",
+    "url": "/Majeed-hub/react-app-2/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.550758221ea8ecaf55a7e54c04bb45dc.js"
+  "/Majeed-hub/react-app-2/precache-manifest.f04752db67bc51bd74188501473a57d0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Majeed-hub/react-app-2/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
